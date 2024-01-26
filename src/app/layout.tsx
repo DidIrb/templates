e4b27131f6537ebdd0bbe99d1a20ad8b26e1805c
DidIrb <irbayedubadida@gmail.com>
2024-01-26 17:44:27 +0300

    initialized the next-mysql template

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "NEXT JS MYSQL TEMPLATE",
+  description: "Build next applications faster with simple template!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
